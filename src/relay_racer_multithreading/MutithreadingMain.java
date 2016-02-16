a4920dbe65d7cd149655ce2b4070e2aa2964cc52
Abhishek Naik <an286@njit.edu>
2016-02-15 23:14:08 -0500

    Added the main file for the project

---
+++ src/relay_racer_multithreading/MutithreadingMain.java
@@ -0,0 +1,10 @@
+package relay_racer_multithreading;
+
+public class MutithreadingMain {
+
+	public static void main(String[] args) {
+		System.out.println("This program is to practice multithreading concept in programming.");
+		
+	}
+
+}
